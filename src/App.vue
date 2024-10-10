d3a9fe70e4924ba2fa89c65892337c41f80f2630
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-10 15:03:33 +0900

    created event card components

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Manager</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard v-for="i in 8" :key="i" />
+    </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
