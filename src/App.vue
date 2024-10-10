93245bb9dd186eaa19975b44baededf5dff759eb
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-10 15:35:26 +0900

    Passed props to dynamically display data to EventCard component

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@
     <h1 class="text-4xl font-medium">Event Manager</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" />
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference 2024"
+        when="2024-05-01"
+        description="Conference about Vue and Javascript"
+      />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
 
 <script setup>
-import EventCard from '@/components/EventCard.vue'
+import EventCard from '@/components/EventCard.vue';
 </script>
