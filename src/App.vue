13529c1965d14b9a2cbfa24cad9fa0b182498ac1
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-11 14:10:56 +0900

    Created LoadingEventCard component and replaced loading text to prevent layout shifts for better UI/UX

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking Manager</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
-      <EventCard
-        v-for="event in events"
-        :key="event.id"
-        :title="event.title"
-        :when="event.date"
-        :description="event.description"
-        @register="console.log('Registered')"
-      />
+    <section class="grid grid-cols-2 gap-8">
+      <template v-if="!eventsLoading">
+        <EventCard
+          v-for="event in events"
+          :key="event.id"
+          :title="event.title"
+          :when="event.date"
+          :description="event.description"
+          @register="console.log('Registered')"
+        />
+      </template>
+
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i"/>
+      </template>
     </section>
-    <section v-else>Loading Events...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i" />
@@ -25,6 +30,7 @@ import { ref, onMounted } from 'vue';
 
 import EventCard from '@/components/EventCard.vue';
 import BookingItem from '@/components/BookingItem.vue';
+import LoadingEventCard from '@/components/LoadingEventCard.vue';
 
 const events = ref([]);
 const eventsLoading = ref(false);
