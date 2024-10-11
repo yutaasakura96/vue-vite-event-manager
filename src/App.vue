f009db44b19d31a7b3a904629288920ddfa11c50
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-11 12:41:13 +0900

    Added loading text for fetching data to improve UI/UX

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking Manager</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
       <EventCard
         v-for="event in events"
         :key="event.id"
         :title="event.title"
-        :date="event.date"
+        :when="event.date"
         :description="event.description"
         @register="console.log('Registered')"
       />
     </section>
+    <section v-else>Loading Events...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i" />
@@ -26,11 +27,16 @@ import EventCard from '@/components/EventCard.vue';
 import BookingItem from '@/components/BookingItem.vue';
 
 const events = ref([]);
+const eventsLoading = ref(false);
 
 const fetchEvents = async () => {
-  const response = await fetch('http://localhost:3001/events');
-  events.value = await response.json();
-  console.log(events.value);
+  eventsLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    events.value = await response.json();
+  } finally {
+    eventsLoading.value = false;
+  }
 };
 
 onMounted(() => {
